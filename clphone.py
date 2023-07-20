b37a3fac0bcde80b139ae044bbfafbda6dfcfe45
Serhii Dulin <64526933+SergDulin@users.noreply.github.com>
2023-07-20 19:08:49 +0300

    GOIT > Курс > Python Developer > Python core > Модуль 11. Магічні методи
    У цій роботі ми продовжили розвивати нашого віртуального асистента  з CLI інтерфейсом.
    
    https://github.com/SergDulin/goit_hw-09/blob/main/goit_hw-10.py - посилання на попереднє завдання.
    
    Створили два файли, до одно помістили класи clphone.py, до другого  функціонал phone11.py
    
    У цьому домашньому завданні ми:
    •	Додамо поле для дня народження Birthday. Це поле не обов'язкове, але може бути тільки одне.
    •	Додамо функціонал роботи з Birthday у клас Record, а саме функцію days_to_birthday, яка повертає кількість днів до наступного дня народження.
    •	Додамо функціонал перевірки на правильність наведених значень для полів Phone, Birthday.
    •	Додамо пагінацію (посторінкове виведення) для AddressBook для ситуацій, коли книга дуже велика і потрібно показати вміст частинами, а не все одразу. Реалізуємо це через створення ітератора за записами.
    Критерії приймання:
    •	AddressBook реалізує метод iterator, який повертає генератор за записами AddressBook і за одну ітерацію повертає представлення для N записів.
    •	Клас Record приймає ще один додатковий (опціональний) аргумент класу Birthday
    •	Клас Record реалізує метод days_to_birthday, який повертає кількість днів до наступного дня народження контакту, якщо день народження заданий.
    •	setter та getter логіку для атрибутів value спадкоємців Field.
    •	Перевірку на коректність веденого номера телефону setter для value класу Phone.
    •	Перевірку на коректність веденого дня народження setter для value класу Birthday.

---
+++ clphone.py
@@ -0,0 +1,104 @@
+from collections import UserDict
+from datetime import datetime
+from typing import Optional
+
+class Field:
+    def __init__(self, value):
+        self._value = None
+        self.value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+        self._value = new_value
+
+    def __str__(self) -> str:
+        return str(self._value)
+
+    def __repr__(self) -> str:
+        return str(self)
+
+class Name(Field):
+    def __init__(self, value):
+        self._value = None
+        self.value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+        self._value = " ".join(word.capitalize() for word in new_value.split())
+
+class Phone(Field):
+    @Field.value.setter
+    def value(self, new_value):
+        if not new_value.startswith("+380") or not new_value[1:].isdigit() or len(new_value) != 13:
+            raise ValueError("Invalid phone number")
+        self._value = new_value
+
+class Birthday(Field):
+    @Field.value.setter
+    def value(self, new_value):
+        if new_value:
+            try:
+                datetime.strptime(new_value, "%d.%m.%Y")
+            except ValueError:
+                raise ValueError("Invalid birthday format. Please use the format 'dd.mm.yyyy'")
+            self._value = new_value
+
+    @property
+    def date(self):
+        if self.value:
+            return datetime.strptime(self.value, "%d.%m.%Y").date()
+        return None
+
+class Record:
+    def __init__(self, name: Name):
+        self.name = name
+        self.phones = []
+        self.birthday: Optional[Birthday] = None
+    def add_phone(self, phone: Phone):
+        if phone.value not in [p.value for p in self.phones]:
+            self.phones.append(phone)
+
+    def change_phone(self, old_phone, new_phone):
+        for idx, p in enumerate(self.phones):
+            if old_phone.value == p.value:
+                self.phones[idx] = new_phone
+
+    def days_to_birthday(self):
+        if self.birthday and self.birthday.date:
+            today = datetime.now().date()
+            next_birthday = datetime(today.year, self.birthday.date.month, self.birthday.date.day).date()
+            if next_birthday < today:
+                next_birthday = datetime(today.year + 1, self.birthday.date.month, self.birthday.date.day).date()
+            days_left = (next_birthday - today).days
+            return days_left
+        else:
+            return None
+
+    def __str__(self) -> str:
+        name_str = str(self.name)
+        phone_numbers = ", ".join(str(p) for p in self.phones)
+        birthday_str = f", {self.birthday}" if self.birthday else ""
+        days_until_birthday = f", Days until the next birthday: {self.days_to_birthday()}" if self.birthday else ""
+        return f"{name_str}: {phone_numbers}{birthday_str}{days_until_birthday}"
+
+class AddressBook(UserDict):
+    def add_record(self, record: Record):
+        self.data[str(record.name)] = record
+        return f"Contact {record.name}: {', '.join(str(p) for p in record.phones)}{', ' + str(record.birthday) if record.birthday else ''} added successfully"
+
+def input_error(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except (KeyError, ValueError, IndexError) as e:
+            return str(e)
+
+    return wrapper
---
+++ phone11.py
@@ -0,0 +1,119 @@
+from clphone import *
+
+address_book = AddressBook()
+
+@input_error
+def add(*args):
+    if len(args) < 2:
+        raise ValueError("Please provide both name and phone number")
+
+    name = Name(args[0])
+    phones = []
+    birthday = None
+
+    for arg in args[1:]:
+        if arg.startswith("+380"):
+            phones.append(Phone(arg))
+        elif arg.count(".") == 2:
+            birthday = Birthday(arg)
+        else:
+            name.value += " " + arg
+
+    rec: Optional[Record] = address_book.get(str(name))
+    if rec:
+        for phone in phones:
+            rec.add_phone(phone)
+        if birthday:
+            rec.birthday = birthday
+    else:
+        rec = Record(name)
+        rec.birthday = birthday
+        for phone in phones:
+            rec.add_phone(phone)
+        address_book.add_record(rec)
+
+    return f"Contact {name}: {', '.join(str(num) for num in rec.phones)}{', ' + str(rec.birthday) if rec.birthday else ''} added successfully"
+
+@input_error
+def change(*args):
+    if len(args) != 3:
+        raise ValueError("Please provide the name, old phone number, and new phone number")
+
+    name = Name(args[0])
+    old_phone = Phone(args[1])
+    new_phone = Phone(args[2])
+
+    rec = address_book.get(str(name))
+    if rec:
+        rec.change_phone(old_phone, new_phone)
+    else:
+        raise KeyError(f"Contact '{name}' not found in address book")
+
+    return f"Old phone {old_phone} changed to {new_phone}"
+
+@input_error
+def phone(*args):
+    if len(args) != 1:
+        raise ValueError("Please provide the name")
+
+    name_str = str(args[0]).capitalize()
+
+    matches = []
+    for record in address_book.values():
+        if name_str in str(record.name).capitalize():
+            matches.append(record)
+
+    result = ""
+    if matches:
+        for record in matches:
+            result += str(record) + "\n"
+    else:
+        raise KeyError(f"Contact '{name_str}' not found in address book")
+
+    return result
+
+@input_error
+def show_all(*args):
+    if not address_book.data:
+        return "There are no contacts saved."
+
+    result = ""
+    for record in address_book.values():
+        result += str(record) + "\n"
+
+    return result
+
+def hello_command(*args):
+    return "How can I help you?"
+
+def goodbye_command(*args):
+    return "Goodbye!"
+
+def no_command(*args):
+    return "Invalid command. Please try again."
+
+command_handlers = {add: ("add", ),
+                    change: ("change",),
+                    phone: ("phone",),
+                    show_all: ("show all", ),
+                    hello_command: ("hello", ),
+                    goodbye_command: ("good bye", "close", "exit")
+                    }
+
+def komand(user_input):
+    for command, keywords in command_handlers.items():
+        for keyword in keywords:
+            if user_input.lower().startswith(keyword):
+                return command, user_input[len(keyword):].strip().split()
+    return no_command, []
+
+def main():
+    while True:
+        user_input = input(">>> ")
+        func, data = komand(user_input)
+        print(func(*data))
+        if func == goodbye_command:
+            break
+
+if __name__ == "__main__":
+    main()
